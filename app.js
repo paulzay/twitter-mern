1ad8f0926c0ca2edeadf1b5908c83bcdf5bb392c
paulzay <paulotieno2@gmail.com>
2021-03-10 14:50:52 +0300

    add routing for user and tweets

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 
 mongoose.connect(db, {
   useNewUrlParser: true,
@@ -17,6 +20,9 @@ mongoose.connect(db, {
 
 app.use(bodyParser.json());
 app.get('/', (req,res)=> res.send('hello world'));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
