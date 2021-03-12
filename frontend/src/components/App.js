28c6101f27e7a0e639c5b00516002e0f3ca78af5
paulzay <paulotieno2@gmail.com>
2021-03-12 22:54:57 +0300

    move appjs inside component folder

---
+++ frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../utils/route_util';
+import { Switch } from 'react-router-dom';
+import MainPage from './main/main_page';
+
+const App = () => (
+    <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+    </Switch>
+);
+
+export default App;
